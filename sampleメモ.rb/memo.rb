fd9399897270d22e189c824f39a6c29fbfa14ded
ikemura23 <banbara.work@gmail.com>
2021-11-18 16:42:09 +0900

    create memo.rb

---
+++ sampleメモ.rb/memo.rb
@@ -0,0 +1,17 @@
+synth_loop = "/Users/k_ikemura/Music/sonic_pi/LCC_CHELL_HOUSE/synth_loops"# Welcome to Sonic Pi
+
+num = 16
+
+# 0, 1, 3, 5, 6, 7, 9, 10,
+# 1: 0の音色違い
+# 5 : 細かい刻みが入っててかっこいい
+# 6 :小さい音から入る、効果音てきな？
+# 9 : きれい、コピーしたい
+# 10 ,9の音違い、きれい、マネしたい
+# 12, 激しい、少し狂ったme
+# 13, いい音、コピーできそう
+# 17: かっこいい、使いたい
+
+sample synth_loop, num ,beat_stretch: 8, amp: 1
+
+##| sample :ambi_sauna, attack_level: 10samp
