67b089ce7483d76572908ad65495c16e03a88118
ikemura23 <banbara.work@gmail.com>
2021-11-21 16:35:26 +0900

    create 2021-11-20.rb

---
+++ 2021-11-20.rb
@@ -0,0 +1,54 @@
+use_bpm 65
+use_random_seed 70000
+
+live_loop :bd do
+  sleep 0.5
+end
+
+live_loop :cymbal_close, sync: :bd do
+  sleep 0.25
+  sample :drum_cymbal_closed, amp: [3, 1].tick, attack: 0.02, rate: 1.4
+end
+
+live_loop :drum, sync: :bd do
+    sample drum ,1, beat_stretch: 4, amp: 2, lpf: 120
+    sleep 4
+  end
+
+notes1 = [68, 70, 73, 75, 79, 80, 82].shuffle
+notes2 = [68, 70, 72, 73, 75, 79, 80, 82].shuffle
+
+live_loop :mero, sync: :bd do
+    use_synth :dtri
+    sl = 0.125
+    a = 0.3
+
+    with_fx :reverb do
+        8.times do
+            synth :dtri, note: notes1.tick, cutoff: rrand(110, 100), sustain: sl, amp: a
+            sleep sl
+        end
+        8.times do
+            synth :dtri, note: notes2.tick, cutoff: rrand(110, 100), sustain: sl, amp: a
+            sleep sl
+        end
+    end
+end
+
+live_loop :bass, sync: :bd do
+  with_fx :distortion do
+    synth :fm, note: :eb2, release: 0.13, amp: 2
+    sleep 0.125
+  end
+end
+
+live_loop :amb, sync: :bd do
+    synth :tech_sows, note: :eb2, attack1, sustain: 8, cutoff: 50, amp: 1
+    sleep 8
+end
+
+sl2 = [-2,0,5,10].shuffle
+live_loop :strings, sync: :bd do
+  use_synth :hollow
+  play chord(:eb4+sl2.tick, :sus2), sustain: 4, release: 10, attack: 2, amp, 6
+end
