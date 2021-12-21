208aad1d65d2daa18c111fdafee5e0698d71dac5
ikemura23 <banbara.work@gmail.com>
2021-12-21 23:59:07 +0900

    add output url 2021-12-21/2021-12-21.rb

--- 2021-12-21/2021-12-21.rb
+++ 2021-12-21/2021-12-21.rb
@@ -1,3 +1,5 @@
+#output https://twitter.com/ikemura23/status/1473297586304225280
+
 drum = "/Users/k_ikemura/Music/sonic_pi/LCKK_SUNNY_HOUSE/LCKK_drum\ loops/LCKK_top\ loops"
 clap = "/Users/k_ikemura/Music/sonic_pi/LCKK_SUNNY_HOUSE/LCKK_one\ shots/LCKK_claps"
 
