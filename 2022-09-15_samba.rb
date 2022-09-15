fe5acdf484970c73256a6380703fd3bdcd09f110
ikemura23 <banbara.work@gmail.com>
2022-09-15 23:08:04 +0900

    add output url

--- 2022-09-15_samba.rb
+++ 2022-09-15_samba.rb
@@ -1,3 +1,4 @@
+# output https://twitter.com/ikemura23/status/1570413839384211458
 use_debug false
 clap = "/Users/k_ikemura/Music/sonic_pi/LCKK_SUNNY_HOUSE/LCKK_one\ shots/LCKK_claps"
 percussion = "/Users/k_ikemura/Music/sonic_pi/LCKK_SUNNY_HOUSE/LCKK_drum\ loops/LCKK_percussion\ loops"
