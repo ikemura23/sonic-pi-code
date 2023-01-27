bcc5e9da3b43b886b199fcb206f0d9812d14c405
ikemura23 <banbara.work@gmail.com>
2023-01-27 10:49:20 +0900

    update

--- 2023/2023-01-26_teckhouse.rb
+++ 2023/2023-01-26_teckhouse.rb
@@ -8,7 +8,7 @@ live_loop :met do
 end
 
 live_loop :kick, sync: :met do
-  sample :bd_haus, amp: 3
+  sample :bd_haus, amp: 2
   sleep 0.5
 end
 
@@ -19,32 +19,40 @@ live_loop :splash, sync: :met do
   sleep 16
 end
 
-live_loop :drum, sync: :met do
-  sample drum_top_loop, 8,beat_stretch: 8 , amp: 2, start: 0, finish: [1, 0.75].ring.tick
+live_loop :sample_drum_top_loop, sync: :met do
+  sample drum_top_loop, 8,beat_stretch: 8 , amp: 1, start: 0, finish: [1, 0.75].ring.tick
   sleep 8
 end
 
-# sample1 7 + sample2 1 の組み合わせが良い
-
 live_loop :sample1, sync: :met do
-  stop
-  num = 7 # 0 1 7 17
+  ##| stop
+  num = 7 #7 17
   sample synth_loop, num ,beat_stretch: 16, amp: 2 #, rate: 0.5
   sleep 16
 end
 
 live_loop :sample2, sync: :met do
   stop
-  num = 1 # 0 1 5 6 7 8
-  sample synth_loop, num ,beat_stretch: 8, amp: 3,start: 0, finish: 0.25
+  sleep 2
+  num = 1 # 0 1
+  sample synth_loop, num ,beat_stretch: 8, amp: 1, start: 0, finish: 0.25
+  sleep 2
+end
+
+live_loop :sample3, sync: :met do
+  stop
+  ##| sleep 2
+  num = 6 #5 6
+  sample synth_loop, num ,beat_stretch: 16, amp: 1, start: 0, finish: 0.25
   sleep 4
 end
 
 base_note = (scale :c1, :minor_pentatonic, num_octaves: 1).shuffle
 base_sleep = [0.25, 0.125]
-base_amp = 2
+base_amp = 1.2
 
 live_loop :base, sync: :met do
+  ##| stop
   use_random_seed 400000 # 400000 3200 3400
   8.times do
     sl = base_sleep.choose
