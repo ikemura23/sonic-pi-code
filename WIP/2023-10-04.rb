e29bad72313f0f8bfb44fd1b522d4647d6432ef8
ikemura23 <banbara.work@gmail.com>
2023-10-04 16:24:50 +0900

    create WIP/2023-10-04

---
+++ WIP/2023-10-04.rb
@@ -0,0 +1,10 @@
+
+## https://www.youtube.com/live/VaHx32PHSg0?si=zVgVl3ozjLsogiXG&t=2275
+
+use_synth :saw
+
+play chord(:c4, :M7, invert: 2), release: 4
+sleep 4
+
+play chord(:c4-2, :M7, invert: 2), release: 4
+sleep 4
