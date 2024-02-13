3cf1241166426064990ff265c145e7f59dc81a21
Kazutaka Ikemura <banbara.work@gmail.com>
2024-02-13 09:57:21 +0900

    update

--- WIP/2024-01-30.rb
+++ WIP/2024-01-30.rb
@@ -2,15 +2,60 @@ live_loop :met do
   sleep 0.5
 end
 
+# drum #############################
+
 live_loop :kick, sync: :met do
-  use_sample_defaults amp: 1.1, lpf: 90
-  sample :bd_tek # :bd_haus :bd_tek :bd_ada
-  sleep 0.5
+  use_sample_defaults amp: 1.3, lpf: 100
+  
+  7.times do
+    sample :bd_tek
+    sleep 0.5
+  end
+  sample :bd_tek
+  sleep 0.25
+  sample :bd_tek
+  sleep 0.25
+end
+
+live_loop :hat, sync: :met do
+  use_sample_defaults hpf: 100, amp: 0.2, rate: 1.3
+  
+  with_fx :reverb, mix: 0.2 do
+    sleep 0.25
+    2.times do
+      sample :hat_star
+      sleep 0.125
+    end
+    sleep 0.25
+    2.times do
+      sample :hat_star
+      sleep 0.125
+    end
+    
+    4.times do
+      sample :hat_star
+      sleep 0.125
+    end
+    sleep 0.25
+    
+    2.times do
+      sample :hat_star
+      sleep 0.125
+    end
+  end
+end
+
+live_loop :cymbal_open, sync: :met do
+  stop
+  
+  sleep 0.25
+  sample :hat_cats, rate: 1, lpf: 130
+  sample :drum_cymbal_open, amp: 0.2, beat_stretch: 1.8, finish: 0.125, hpf: 90#, rate: 1.1
+  sleep 0.25
 end
 
 live_loop :snare_loop, sync: :met do
   ##| stop
-  ##| use_sample_defaults lpf: 100
   
   co = 90
   at = 0.00
@@ -20,36 +65,35 @@ live_loop :snare_loop, sync: :met do
     
     sleep 0.5
     
-    in_thread do
-      sample :sn_generic, rate: r+1, cutoff: co, attack: at, amp: 0.1
-      sleep 0.01
-      sample :sn_generic, rate: r, start: 0.03, cutoff: co#, pan: 0.2, attack: at
-    end
+    sample :sn_generic, rate: r+1, cutoff: co, attack: at, amp: 0.2
+    sample :sn_generic, rate: r, start: 0.03, cutoff: co#, pan: 0.2, attack: at
+    
     sleep 0.5
   end
 end
 
 
+# Synth ############################
+
 live_loop :synth1, sync: :met do
   ##| stop
   
-  use_synth_defaults release: 2, amp: 1
+  use_synth_defaults release: 1.25, amp: 1.2
   
-  cd = chord(:ab3, :M7, invert: +2)
+  cd = chord(:ab2, :M7, invert: +2)
   
-  with_fx :reverb, mix: 0.9, room: 0.8 do
-    ##| with_fx :slicer, phase: 0.5, mix: 0.5, invert_wave: 1, smooth: 0.5 do
+  with_fx :reverb, mix: 0.8, room: 0.8 do
     
     with_fx :lpf, cutoff: 90 do
-      use_synth :dtri
-      play cd, amp: 0.5
-      use_synth :sine
-      play chord(:ab3, :M7, invert: 1)
-      sleep 2
-      
-      ##| play cd
-      sleep 2
-      ##| end
+      2.times do
+        use_synth :dtri
+        play cd, amp: 0.5
+        use_synth :sine
+        play chord(:ab3, :M7, invert: 1)
+        sleep 2
+        
+        sleep 2
+      end
     end
   end
 end
@@ -57,24 +101,27 @@ end
 
 live_loop :synth2, sync: :met do
   ##| stop
-  use_synth :dsaw
-  use_synth_defaults release: 0.25, amp: 1, cutoff: 90
+  use_synth :saw
+  use_synth_defaults release: 0.25, amp: 1.5, cutoff: 90
   
   with_fx :reverb, mix: 0.5, room: 0.2 do
     with_fx :echo, decay: 6, phase: 0.375, max_phase: 5 do
-      with_fx :hpf, cutoff: 80 do
-        sleep 1
-        play chord(:ab4, :M7, invert: -2)
-        sleep 3
+      with_fx :lpf, cutoff: 100 , mix: 1 do
+        sleep 1.25
+        play chord(:ab2, :M7, invert: 0)
+        sleep 2.75
       end
       
     end
   end
 end
 
+
+# bass ###########################
+
 live_loop :bass, sync: :met do
   ##| stop
-  use_synth_defaults amp: 1.3, release: 0.2, sustain: 0.1
+  use_synth_defaults amp: 1.5, release: 0.2, sustain: 0.1
   use_synth :fm
   ##| with_fx :lpf, cutoff: 70 do
   key = :f2
